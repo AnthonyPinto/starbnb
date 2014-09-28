9191a35d89f53f697530833d6c247300e49831ff
Anthony Pinto <anthonyepinto@gmail.com>
2014-09-27 22:13:44 -0700

    add styling/features to spaceport show

--- app/assets/javascripts/views/show_spaceport.js
+++ app/assets/javascripts/views/show_spaceport.js
@@ -6,10 +6,17 @@ Starbnb.Views.ShowSpaceport = Backbone.CompositeView.extend({
   initialize: function () {
     this.listenTo(this.model, 'sync', this.render);
   },
+	
+  initDate: function () {
+    this.$('.datetimepicker').datetimepicker({
+      pickTime: false
+    });
+  },
   
   render: function () {
     var content = this.template({spaceport: this.model});
     this.$el.html(content);
+		this.initDate()
     return this;
   }
 });
